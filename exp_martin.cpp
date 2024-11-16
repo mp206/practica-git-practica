c1471629e5c3877fb447ca13cef526fcc63280a3
Martín Eduardo Plascencia Lares <plascencialares16@gmail.com>
2024-11-15 23:33:08 -0700

    Modificacion al codigo

--- exp_martin.cpp
+++ exp_martin.cpp
@@ -1,8 +1,19 @@
  #include <iostream>
  #include <stdio.h>
+ #include <cmath>
 
  int main (){
-    
+    int i, x, n;
+    bool b;
+    cout<< "Ingrese el valor de n: ";
+    cin >> n;
+    while (i=1, i++){
+        if () then{
+
+        }
+    } else {
+        
+    }
     return 0;
  }
 
